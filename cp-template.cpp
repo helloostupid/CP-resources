b7dde55b99315c1bb62a919b161bf51cd3d8eb51
Sonal Gholap <sonal.gholap@spit.ac.in>
2024-03-17 13:22:51 +0530

    initial commit

---
+++ cp-template.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define ull unsigned long long
+#define lld long double
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define vi vector<ll>
+#define pi pair<ll,ll>
+#define ff first
+#define ss second
+#define mod 1000000007
+#define MOD 998244353
+#define all(x) (x).begin(), (x).end()
+#define for_(i,a,b) for(int i = a; i < b; i++) // [a,b)
+#define PI 3.141592653589793238462
+#define pb push_back
+#define cy cout << "Yes" << endl
+#define cn cout << "No" << endl
+#define endl "\n"
+#define float double
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+int main()
+{
+    fast;
+
+    ll t;
+    cin>>t;
+    
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        
+    }
+ 
+    return 0;
+}
